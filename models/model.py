ea6c0d3af5cb7ee506c12297033f224c1714ceac
sfscs <jacobneilgustafson@gmail.com>
2016-02-22 00:54:39 -0800

    slimmed down model plus mm schema

---
+++ models/model.py
@@ -0,0 +1,99 @@
+import logging
+import pprint
+import psycopg2
+import psycopg2.extras
+import yaml
+import abc
+import uuid
+from uuid import UUID
+from datetime import datetime
+
+
+psycopg2.extras.register_uuid()
+
+class Provider:
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, configfile=None):
+        if configfile is None:
+            self.configfile = "/opt/bernie/config.yml"
+        else:
+            self.configfile = configfile
+
+        self.config = self.get_config()
+        self.db = None
+
+    def connect (self):
+        c = self.config["postgresql"]
+        db = psycopg2.connect("host=" + c["host"] + " dbname=" + c["dbname"] + " user=" + c["dbuser"] + " password=" + c["dbpass"])
+        db.autocommit = True
+        return db
+
+    def get_config(self):
+        try:
+            with open(self.configfile, 'r') as f:
+                conf = yaml.load(f)
+        except IOError:
+            msg = "Could not open config file: {0}"
+            logging.info(msg.format(self.configfile))
+            raise
+        else:
+            return conf
+
+    def get_db (self):
+        if self.db is None:
+            self.db = self.connect()
+            # Time Zone only needs to be set once per connection
+            cur = self.db.cursor()
+            cur.execute ("SET TIME ZONE 'UTC'")
+            cur.close()
+        return self.db
+
+    def get_db_cursor (self):
+        """
+        From the psycopg docs:
+        But note that cursors used to fetch result sets will cache the data and
+        use memory in proportion to the result set size. Our suggestion is to
+        almost always create a new cursor and dispose old ones as soon as the
+        data is not required anymore (call close() on them.) The only exception
+        are tight loops where one usually use the same cursor for a whole bunch
+        of INSERTs or UPDATEs.
+        """
+        db = self.get_db()
+        # RealDictCursor so we can access our results by column-name
+        return db.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+
+    def get (self, size = 100):
+        """
+        Yields all objects from the current table.
+        The 'size' parameter controls how many rows are fetched from
+        the server at a time.
+        """
+        with self.get_db_cursor() as cur:
+            query = cur.mogrify("SELECT * FROM %s" % (self.table_name))
+            cur.execute (query)
+            res = cur.fetchmany(size)
+
+            while len(res) > 0:
+                for r in res:
+                    yield self.make_model (r)
+                res = cur.fetchmany(size)
+
+    @abc.abstractmethod
+    def make_model (self, props):
+        """
+        Take a dictionary of properties and converts it to the relevant
+        Model object
+        """
+        return
+
+class Model:
+    def __init__(self, props):
+        for k,v in props.iteritems():
+            # Ugh, `id` is a builtin python function and its use as a variable
+            # or property name is frowned upon. The recommended convention is
+            # to use `id_` instead
+            if k == 'id':
+                self.id_ = v
+            else:
+                setattr (self, k, v)
---
+++ models/news.py
@@ -0,0 +1,89 @@
+from __future__ import generators
+import psycopg2
+import psycopg2.extras
+import yaml
+import model
+from marshmallow_jsonapi import Schema, fields
+
+class NewsProvider (model.Provider):
+    table_name = "news"
+
+    def exists (self, uuid):
+        """ Returns True if record with uuid exists """
+        return self.read (uuid) is not None
+
+    def exists_by_news_id (self, news_id):
+        """ Returns True if record with news_id exists """
+        return self.read_by_news_id (news_id) is not None
+
+    def exists_by_title_news_type (self, title, news_type):
+        """ Returns True if record with title and news_type exists """
+        return self.read_by_title_news_type (title, news_type) is not None
+
+    def make_model (self, props):
+        return News (props)
+
+    def get_all(self):
+        with self.get_db_cursor() as cur:
+            cur.execute("SELECT * FROM news ORDER BY timestamp_publish DESC")
+            res = cur.fetchall()
+            returns = []
+            for record in res:
+                if record is not None:
+                    returns.append(News(record))
+            return returns
+
+    def get_all_languages (self):
+        with self.get_db_cursor() as cur:
+            cur.execute ("SELECT DISTINCT lang FROM news", ())
+            return map (lambda x: x['lang'], cur)
+
+    def get_all_sites (self):
+        with self.get_db_cursor() as cur:
+            cur.execute ("SELECT DISTINCT site FROM news", ())
+            return map (lambda x: x['site'], cur)
+
+    def read (self, uuid):
+        with self.get_db_cursor() as cur:
+            cur.execute("SELECT * FROM news WHERE uuid = (%s)", (uuid,))
+            res = cur.fetchone()
+
+            if res is not None:
+                return News (res)
+            else:
+                return None
+
+    def read_by_news_id (self, news_id):
+       with self.get_db_cursor() as cur:
+           cur.execute("SELECT * FROM news WHERE news_id = (%s)", (news_id,))
+           res = cur.fetchone()
+
+           if res is not None:
+               return News (res)
+           else:
+               return None
+
+    def read_by_title_news_type (self, title, news_type):
+        with self.get_db_cursor() as cur:
+            cur.execute("SELECT * FROM news WHERE title = (%s) AND news_type = (%s)", (title, news_type))
+            res = cur.fetchone()
+
+            if res is not None:
+                return News (res)
+            else:
+                return None
+
+class News (model.Model):
+    object_type = 'news'
+
+class NewsSchema(Schema):
+    id = fields.UUID(attribute="uuid")
+    title = fields.Str()
+    body_markdown = fields.Str()
+    excerpt = fields.Str()
+    timestamp_publish = fields.DateTime("%Y-%m-%dT%H:%M:%S+00:00")
+    url = fields.Url()
+    image_url = fields.Url()
+    lang = fields.Str()
+    class Meta:
+        type_ = "news"
